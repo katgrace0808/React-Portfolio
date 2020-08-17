e480fbf9ad7efa885f77913e8d65d73b689abcc7
Kathryn Teall <katgrace0808@gmail.com>
2020-08-16 21:11:09 -0700

    Updates

---
+++ precache-manifest.013be0289827a9e3ff3a7e238242a885.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759bfb697275d15a05320c272c16eaa3",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "fda5560ffb95d37b2d61",
+    "url": "/react-portfolio/static/css/main.ffce9130.chunk.css"
+  },
+  {
+    "revision": "412a83c508d1a26eff71",
+    "url": "/react-portfolio/static/js/2.ddc782c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.ddc782c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda5560ffb95d37b2d61",
+    "url": "/react-portfolio/static/js/main.177883a4.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "3c7a8e6a4697cb87442583f490d189ea",
+    "url": "/react-portfolio/static/media/Food-Truck-screenshot.3c7a8e6a.PNG"
+  },
+  {
+    "revision": "9890b40f92fdd90af66579d2d3287edd",
+    "url": "/react-portfolio/static/media/deskpc.9890b40f.jpg"
+  },
+  {
+    "revision": "d61aa51f130538c0e4aa30858feefecf",
+    "url": "/react-portfolio/static/media/headshot.d61aa51f.jpg"
+  },
+  {
+    "revision": "1ce92d6a4681173c22143d3909e581cf",
+    "url": "/react-portfolio/static/media/informatics.1ce92d6a.jpg"
+  },
+  {
+    "revision": "56885f26ae436749c6cca9c30a6bdace",
+    "url": "/react-portfolio/static/media/r2d2.56885f26.jpg"
+  }
+]);
